5cad9cf439af24408f85e3cff2175ae915f01232
Tecu23 <andrei.teculescu@gmail.com>
2024-12-11 16:11:06 +0200

    add types

---
+++ internal/types/search.go
@@ -0,0 +1,41 @@
+package types
+
+import (
+	"time"
+
+	"github.com/Tecu23/argov2/pkg/board"
+	"github.com/Tecu23/argov2/pkg/move"
+)
+
+type UciScore struct {
+	Centipawns int
+	Mate       int
+}
+
+type SearchInfo struct {
+	Score    UciScore
+	Depth    int
+	Nodes    int64
+	Time     time.Duration
+	MainLine []move.Move
+}
+
+type LimitsType struct {
+	Ponder         bool
+	Infinite       bool
+	WhiteTime      int
+	BlackTime      int
+	WhiteIncrement int
+	BlackIncrement int
+	MoveTime       int
+	MovesToGo      int
+	Depth          int
+	Nodes          int
+	Mate           int
+}
+
+type SearchParams struct {
+	Boards   []board.Board
+	Limits   LimitsType
+	Progress func(si SearchInfo)
+}
