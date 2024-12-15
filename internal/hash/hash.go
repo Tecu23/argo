2581c5e7315dd86cba917b295a52c4df89bed929
Tecu23 <andrei.teculescu@gmail.com>
2024-12-15 19:09:39 +0200

    add zobrist hashing function and table

---
+++ internal/hash/hash.go
@@ -0,0 +1,52 @@
+package hash
+
+import "math/rand"
+
+const (
+	PieceSquareCount = 12 * 64 // 12 piece types * 64 squares
+	CastlingCount    = 4       // 4 castling rights
+	EnPassantCount   = 8       // 8 files for en passant
+	SideCount        = 1       // Side to Move
+)
+
+type ZobristHash struct {
+	PieceSquare [PieceSquareCount]uint64 // Random numbers for each piece on each square
+	Castling    [CastlingCount]uint64    // Random numbers for castling rights
+	EnPassant   [EnPassantCount]uint64   // Random numbers for en passant files
+	Side        uint64                   // Random number for side to move
+}
+
+var HashTable *ZobristHash
+
+func init() {
+	// use a fixed seed for reproducibility
+	const seed = 23
+
+	HashTable = NewZobristHash(seed)
+}
+
+func NewZobristHash(seed int64) *ZobristHash {
+	rng := rand.New(rand.NewSource(seed))
+
+	z := &ZobristHash{}
+
+	// Initialize piece square values
+	for i := 0; i < PieceSquareCount; i++ {
+		z.PieceSquare[i] = rng.Uint64()
+	}
+
+	// Initialize castlings values
+	for i := 0; i < CastlingCount; i++ {
+		z.Castling[i] = rng.Uint64()
+	}
+
+	// Initialize en passant values
+	for i := 0; i < EnPassantCount; i++ {
+		z.EnPassant[i] = rng.Uint64()
+	}
+
+	// Initialize side to move value
+	z.Side = rng.Uint64()
+
+	return z
+}
