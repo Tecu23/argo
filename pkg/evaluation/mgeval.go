5f09c0ee64f7dc3dcd601cd5abc409906c2eff7e
Tecu23 <andrei.teculescu@gmail.com>
2024-12-24 13:00:40 +0200

    finish passed pawn evaluation

--- pkg/evaluation/mgeval.go
+++ pkg/evaluation/mgeval.go
@@ -1628,19 +1628,133 @@ func PassedMg(b *board.Board) int {
 // PassedLeverable returns candidate passers without candidate passers w/o
 // feasible lever
 func PassedLeverable(b *board.Board, sq int) int {
+	if b.CandidatePassed(color.WHITE) == 0 {
+		return 0
+	}
+
+	rank := sq / 8
+	file := sq % 8
+
+	if !b.Bitboards[BP].Test((rank-1)*8 + file) {
+		return 1
+	}
+
+	mirror := b.Mirror()
+	for i := -1; i <= 1; i += 2 {
+		sq1 := rank*8 + file + i
+		sq2 := (7-rank)*8 + file + i
+
+		if (b.Bitboards[WP].Test((rank+1)*8+file+i) && rank < 7 && file+i >= 0 && file+i <= 7) &&
+			(!b.Occupancies[color.BLACK].Test(rank+8+file+i) && file+i >= 0 && file+i <= 7) &&
+			(Attack(b, sq1) > 0 || Attack(mirror, sq2) <= 1) {
+			return 1
+		}
+	}
+
 	return 0
 }
 
+// PassedRank is a bonus according to the rank of a passed pawn
 func PassedRank(b *board.Board, sq int) int {
-	return 0
+	if PassedLeverable(b, sq) == 0 {
+		return 0
+	}
+
+	rank := sq / 8
+
+	return 7 - rank
 }
 
+// PassedFile is a bonus according to the file of a passed pawn
 func PassedFile(b *board.Board, sq int) int {
-	return 0
+	if PassedLeverable(b, sq) == 0 {
+		return 0
+	}
+
+	file := sq % 8
+
+	return min(file, 7-file)
 }
 
+// PassedBlock adds bonus if passed pawn is free to advance. Bonus is
+// adjusted based on attacked and defended status of block square and
+// entire path in front of path
 func PassedBlock(b *board.Board, sq int) int {
-	return 0
+	if PassedLeverable(b, sq) == 0 {
+		return 0
+	}
+
+	rank := sq / 8
+	file := sq % 8
+
+	if 8-rank < 4 {
+		return 0
+	}
+
+	if b.Occupancies[color.BOTH].Test((rank-1)*8 + file) {
+		return 0
+	}
+
+	r := 7 - rank
+	w := 0
+	if r > 2 {
+		w = 5*r - 13
+	}
+
+	mirror := b.Mirror()
+
+	defended, unsafe, wunsafe, defended1, unsafe1 := 0, 0, 0, 0, 0
+
+	for y := rank - 1; y >= 0; y++ {
+		if Attack(b, y*8+file) > 0 {
+			defended++
+		}
+
+		if Attack(mirror, (7-y)*8+file) > 0 {
+			unsafe++
+		}
+
+		if Attack(mirror, (7-y)*8+file-1) > 0 && file > 0 {
+			wunsafe++
+		}
+
+		if Attack(mirror, (7-y)*8+file+1) > 0 && file < 7 {
+			wunsafe++
+		}
+
+		if y == rank-1 {
+			defended1 = defended
+			unsafe1 = unsafe
+		}
+	}
+
+	for y := rank + 1; y < 8; y++ {
+		if b.Bitboards[WR].Test(y*8+file) || b.Bitboards[WQ].Test(y*8+file) {
+			defended1 = rank
+			defended = rank
+		}
+
+		if b.Bitboards[BR].Test(y*8+file) || b.Bitboards[BQ].Test(y*8+file) {
+			unsafe1 = rank
+			unsafe = rank
+		}
+	}
+
+	k := 0
+
+	if unsafe == 0 && wunsafe == 0 {
+		k = 35
+	} else if unsafe == 0 {
+		k = 20
+	} else if unsafe1 == 0 {
+		k = 9
+	}
+
+	if defended1 != 0 {
+		k += 5
+	}
+
+	return k * w
 }
 
 // Space computes the space evaluation for a given side. The Space are
